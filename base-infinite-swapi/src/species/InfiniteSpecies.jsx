6171c9b395fdec55250b981bd6fdc6277b7aff97
ChaDongmin <cdm1263@gmail.com>
2023-09-02 06:37:54 +0900

    강의 1~47

--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
+import { useInfiniteQuery } from "react-query";
 
 const initialUrl = "https://swapi.dev/api/species/";
 const fetchUrl = async (url) => {
@@ -9,5 +10,42 @@ const fetchUrl = async (url) => {
 
 export function InfiniteSpecies() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    hasNextPage,
+    fetchNextPage,
+    isError,
+    error,
+    isLoading,
+    isFetching,
+  } = useInfiniteQuery(
+    "sw-spices",
+    ({ pageParams = initialUrl }) => fetchUrl(pageParams),
+    { getNextPageParam: (lastPage) => lastPage.next || undefined }
+  );
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div>Error! {error.toString()}</div>;
+
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((pageData) => {
+          return pageData.results.map((species) => {
+            return (
+              <>
+                <Species
+                  key={species.name}
+                  name={species.name}
+                  language={species.language}
+                  averageLifespan={species.averageLifespan}
+                />
+              </>
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
--- base-lazy-days/client/src/components/app/App.tsx
+++ base-lazy-days/client/src/components/app/App.tsx
@@ -1,5 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { ReactElement } from 'react';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+// eslint-disable-next-line import/no-unresolved
+import { queryClient } from 'react-query/queryClient';
 
 import { theme } from '../../theme';
 import { Loading } from './Loading';
@@ -9,9 +13,12 @@ import { Routes } from './Routes';
 export function App(): ReactElement {
   return (
     <ChakraProvider theme={theme}>
-      <Navbar />
-      <Loading />
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <Loading />
+        <Routes />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
